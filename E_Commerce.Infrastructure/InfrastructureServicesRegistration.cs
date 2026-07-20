db279527c82e9062e25810e873af2784fdd745ae
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-07-20 23:16:51 +0300

    Added data seeding for products, brands and types.

--- E_Commerce.Infrastructure/InfrastructureServicesRegistration.cs
+++ E_Commerce.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,6 @@
-﻿using E_Commerce.Infrastructure.Data;
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Infrastructure.Data;
+using E_Commerce.Infrastructure.DataSeeding;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +20,10 @@ namespace E_Commerce.Infrastructure
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
+
+            //services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+            services.AddKeyedScoped<IDataSeeder, CatalogDataSeeder>("Catalog");
+
             return services;
         }
     }
