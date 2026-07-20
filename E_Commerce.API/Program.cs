1691276200be79d2b01a84423d3be90668b3326a
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-07-20 05:58:53 +0300

    created Product entites, configurations, StoreDbContext and extended DI services for Infrastructure

--- E_Commerce.API/Program.cs
+++ E_Commerce.API/Program.cs
@@ -1,4 +1,6 @@
 
+using E_Commerce.Infrastructure;
+
 namespace E_Commerce.API
 {
     public class Program
@@ -10,6 +12,7 @@ namespace E_Commerce.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
