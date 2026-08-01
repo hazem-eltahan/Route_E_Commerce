0f59f555fc239bb6c62d861b16ac506ca710fa24
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-08-02 02:18:46 +0300

    added picture url resolver in mapper and UseStaticFiles middle-ware

--- E_Commerce.API/Program.cs
+++ E_Commerce.API/Program.cs
@@ -1,8 +1,10 @@
 
 using E_Commerce.API.Extensions;
 using E_Commerce.Application;
+using E_Commerce.Application.Profiles;
 using E_Commerce.Domain.Contracts;
 using E_Commerce.Infrastructure;
+using Microsoft.Extensions.FileProviders;
 
 namespace E_Commerce.API
 {
@@ -17,6 +19,8 @@ namespace E_Commerce.API
             builder.Services.AddControllers();
             builder.Services.AddInfrastructureServices(builder.Configuration);
             builder.Services.AddApplicationServices();
+            builder.Services.Configure<UrlSettings>(builder.Configuration.GetSection("UrlSettings"));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -31,6 +35,12 @@ namespace E_Commerce.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Files")),
+                RequestPath = "/Files"
+            });
 
             app.UseHttpsRedirection();
 
