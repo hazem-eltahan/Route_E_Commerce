02b737786ed662f314eb159a4b6642ada5f46644
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-09-19 00:13:23 +0300

    Created redis cache repository, service and attribute

---
+++ E_Commerce.API/Attributes/RedisCacheAttribute.cs
@@ -0,0 +1,61 @@
+﻿using E_Commerce.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
+
+namespace E_Commerce.API.Attributes
+{
+    public class RedisCacheAttribute : ActionFilterAttribute
+    {
+        private readonly int _durationInSec;
+
+        public RedisCacheAttribute(int durationInSec = 60)
+        {
+            _durationInSec = durationInSec;
+        }
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<ICacheService>();
+
+            var cacheKey = CreateCacheKey(context.HttpContext.Request);
+
+            var data = await cacheService.GetDataAsync(cacheKey);
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                context.Result = new ContentResult()
+                {
+                    Content = data,
+                    ContentType = "application/json",
+                    StatusCode = StatusCodes.Status200OK
+                };
+                return;
+            }
+
+            var excutedContext = await next.Invoke();
+
+            if (excutedContext.Result is OkObjectResult { Value: not null } ok)
+            {
+                await cacheService.SetDataAsync(cacheKey, ok.Value, TimeSpan.FromSeconds(_durationInSec));
+            }
+        }
+
+        private static string CreateCacheKey(HttpRequest request)
+        {
+            var key = new StringBuilder();
+            key.Append(request.Path);
+
+            if (request.Query.Any())
+            {
+                key.Append('?');
+
+                foreach (var (k, v) in request.Query.OrderBy(x => x.Key))
+                {
+                    key.Append(k).Append('=').Append(v).Append('&');
+                }
+            }
+            return key.ToString();
+        }
+    }
+}
---
+++ E_Commerce.Infrastructure/Repositories/CacheRepository.cs
@@ -0,0 +1,29 @@
+﻿using E_Commerce.Domain.Contracts;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    internal class CacheRepository : ICacheRepository
+    {
+        private readonly IDatabase _database;
+        public CacheRepository(IConnectionMultiplexer connection)
+        {
+            _database = connection.GetDatabase();
+        }
+        public async Task<string?> GetAsync(string cachekey, CancellationToken ct = default)
+        {
+            var value = await _database.StringGetAsync(cachekey);
+            return value.IsNullOrEmpty ? null : value.ToString();
+        }
+
+        public async Task SetAsync(string cachekey, string cacheValue, TimeSpan? TTL = null, CancellationToken ct = default)
+        {
+            await _database.StringSetAsync(cachekey, cacheValue, TTL ?? TimeSpan.FromDays(2));
+        }
+    }
+}
